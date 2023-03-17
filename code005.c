65f570ffa9def4be1b123bff1f1241753c9e7e9e
Takehiro Watanabe <mogu.224@gmail.com>
2023-03-17 17:27:17 +0900

    005, 006

---
+++ code005.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("29+29の計算\n");
+    printf("%d\n", 29 + 29);
+    return 0;
+}
---
+++ code006.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    x = 6;
+    printf("%d\n", x * x * 3);
+}
