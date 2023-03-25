0f7804415c343c291bcef2c15987ec72e2e4faec
Takehiro Watanabe <mogu.224@gmail.com>
2023-03-25 15:47:07 +0900

    Add srand setting

--- code07_04.c
+++ code07_04.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
+#include <stdlib.h> // srand
 #include <time.h>
 
+// String型を作成(C言語にはString型が存在しない)
+typedef char String[1024];
+
+int main(void)
+{
+    // 乱数生成(乱数のタネを変更する*これを行わないと乱数が同じ数値になる)
+    srand((unsigned)time(NULL));
+}
