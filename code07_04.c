8b062603d263d0c0328b9436eebd729a911d2cf3
Takehiro Watanabe <mogu.224@gmail.com>
2023-04-18 21:21:18 +0900

    Add printf

--- code07_04.c
+++ code07_04.c
@@ -78,6 +78,7 @@ int main(void)
                 for (int i = 0; i < 3; i++) {
                     printf("%d", answer[i]);
                 }
+                printf("でした!\n");
             }
         }
 
